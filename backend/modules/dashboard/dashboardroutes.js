4c8b7cf69527cfadc96602680a0e32eec1732cb6
KAVIVARMAN100 <113365297+KAVIVARMAN100@users.noreply.github.com>
2023-10-31 19:24:19 +0530

    Update dashboardroutes.js

--- backend/modules/dashboard/dashboardroutes.js
+++ backend/modules/dashboard/dashboardroutes.js
@@ -5,10 +5,23 @@ import { addnewbook,  deletebook, getallbooks,  getsinglebook, updatebook } from
 
 const router = express.Router();
 
+
+//READING ALL BOOKS FROM DB ------------http://localhost:8000/allbooks
 router.get('/allbooks',getallbooks)
+
+
+//GETTING A SINGLE BOOK-----------http://localhost:8000/book/653fdfa214c1facaa0595afe
 router.get('/book/:id',getsinglebook)
+
+//INSERTING A BOOK -----http://localhost:8000/book/new
 router.post('/book/new',addnewbook)
+
+
+//UPDATING A BOOK------------------http://localhost:8000/book/:ID
 router.put('/book/:id',updatebook)
+
+
+//DELETING  a book--------------------http://localhost:8000/book/6540fe59fdebab2a1df45ac
 router.delete('/book/:id',deletebook)
 
 
